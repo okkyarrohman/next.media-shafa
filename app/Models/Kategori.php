6a244c3fbe58509c4ab102c1c42d17d3591d7247
okkyarrohman <okkyzee@gmail.com>
2024-01-19 10:19:24 +0700

    Kategori Kuis Model and Migrations

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Kategori extends Model
 {
     use HasFactory;
+
+    protected $table = 'kategoris';
+
+    protected $fillable = [
+        'kuis',
+        'tenggat',
+    ];
 }
--- database/migrations/2024_01_19_024310_create_kategoris_table.php
+++ database/migrations/2024_01_19_024310_create_kategoris_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
+            $table->string('kuis')->nullable();
+            $table->string('tenggat')->nullable();
             $table->timestamps();
         });
     }
