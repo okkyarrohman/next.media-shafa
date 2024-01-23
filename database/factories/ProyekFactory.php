bc16cb509b6146319914636b3f5537f1d21f60e6
okkyarrohman <okkyzee@gmail.com>
2024-01-23 13:00:08 +0700

    Proyek-Seeder Update

--- database/factories/ProyekFactory.php
+++ database/factories/ProyekFactory.php
@@ -18,8 +18,14 @@ class ProyekFactory extends Factory
     {
         return [
             'nama' => $this->faker->name(),
-            'proses' => $this->faker->sentence(),
-            'file' => $this->faker->imageUrl(),
+            'proses1' => $this->faker->randomElement(['Deskripsi Proyek']),
+            'file1' => $this->faker->imageUrl(),
+            'proses2' => $this->faker->randomElement(['Perencanaan Proyek']),
+            'file2' => $this->faker->imageUrl(),
+            'proses3' => $this->faker->randomElement(['Perancangan Proyek']),
+            'file3' => $this->faker->imageUrl(),
+            'proses4' => $this->faker->randomElement(['Pengembangan Proyek']),
+            'file4' => $this->faker->imageUrl(),
         ];
     }
 }
