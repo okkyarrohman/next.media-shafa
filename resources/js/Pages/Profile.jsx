a4844d3110d1bb4d9ba6bf60aad462a96adcdfa1
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-01-30 21:14:15 +0700

    Profile

--- resources/js/Pages/Profile.jsx
+++ resources/js/Pages/Profile.jsx
@@ -1,14 +1,29 @@
 import MainLayout from "@/Layouts/MainLayout";
 import SubLayout from "@/Layouts/SubLayout";
-import { Head } from "@inertiajs/react";
+import { Head, useForm, usePage } from "@inertiajs/react";
 import { logoNextMedia } from "../../assets";
 import TextInput from "@/Components/TextInput";
 
-export default function Profile() {
+export default function Profile({ auth }) {
     const url = "https://source.unsplash.com/random/300x300";
+    const user = usePage().props.auth.user;
+
+    const { data, setData, patch } = useForm({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        tanggal_lahir: user.tanggal_lahir,
+        alamat: user.alamat,
+        telepon: user.telepon,
+    });
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        patch(route("profile.update"));
+    };
 
     return (
-        <SubLayout>
+        <SubLayout auth={auth}>
             <Head title="Profile" />
             <img
                 className="w-60 absolute right-0 top-0"
@@ -23,27 +38,27 @@ export default function Profile() {
                     <div className="w-60">
                         <img src={url} alt="" />
                         <p className="font-semibold text-2xl text-center">
-                            Mashi Kreyleight Amsburg Hendrickson
+                            {user.name}
                         </p>
                     </div>
                     <form className="w-full">
                         <div className="mb-3">
                             <label
                                 className="block mb-1 font-semibold"
-                                htmlFor="tanggal"
+                                htmlFor="tanggal_lahir"
                             >
                                 Tanggal Lahir
                             </label>
                             <TextInput
-                                id="tanggal"
+                                id="tanggal_lahir"
                                 type="date"
-                                name="tanggal"
+                                name="tanggal_lahir"
                                 isFocused={true}
                                 className="w-full bg-abu border border-[#ABABAB]"
-                                // value={data.tanggal}
-                                // onChange={(e) =>
-                                //     setData("tanggal", e.target.value)
-                                // }
+                                value={data.tanggal_lahir}
+                                onChange={(e) =>
+                                    setData("tanggal_lahir", e.target.value)
+                                }
                             />
                         </div>
                         <div className="mb-3">
@@ -59,32 +74,35 @@ export default function Profile() {
                                 name="alamat"
                                 placeholder="Masukkan Alamat..."
                                 className="w-full bg-abu border border-[#ABABAB]"
-                                // value={data.alamat}
-                                // onChange={(e) =>
-                                //     setData("alamat", e.target.value)
-                                // }
+                                value={data.alamat}
+                                onChange={(e) =>
+                                    setData("alamat", e.target.value)
+                                }
                             />
                         </div>
                         <div className="mb-16">
                             <label
                                 className="block mb-1 font-semibold"
-                                htmlFor="telp"
+                                htmlFor="telepon"
                             >
                                 No. Telepon
                             </label>
                             <TextInput
-                                id="telp"
+                                id="telepon"
                                 type="text"
-                                name="telp"
+                                name="telepon"
                                 placeholder="Masukkan No. Telepon..."
                                 className="w-full bg-abu border border-[#ABABAB]"
-                                // value={data.telp}
-                                // onChange={(e) =>
-                                //     setData("telp", e.target.value)
-                                // }
+                                value={data.telepon}
+                                onChange={(e) =>
+                                    setData("telepon", e.target.value)
+                                }
                             />
                         </div>
-                        <button className="bg-first rounded py-2 px-16 text-white font-semibold mx-auto w-fit block">
+                        <button
+                            className="bg-first rounded py-2 px-16 text-white font-semibold mx-auto w-fit block"
+                            onClick={handleFormSubmit}
+                        >
                             Edit Profile
                         </button>
                     </form>
