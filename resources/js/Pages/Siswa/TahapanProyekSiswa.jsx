3f0ca97d20931892b7b7c5b23a51b09774945a91
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-01-30 09:00:11 +0700

    progress proyek siswa

--- resources/js/Pages/Siswa/TahapanProyekSiswa.jsx
+++ resources/js/Pages/Siswa/TahapanProyekSiswa.jsx
@@ -13,8 +13,18 @@ export default function TahapanProyekSiswa({ auth }) {
         (hasilProyek) => hasilProyek.user_id == auth.user.id
     );
 
+
+    // ======================================== HANDLE DATA 1
+    // const { data, setData, post, patch } = useForm({
+    //     proyek_id: proyeks[0].id,
+    //     answer1: "",
+    //     answer2: null,
+    //     answer3: null,
+    //     answer4: null,
+    // });
+
+    // ======================================== HANDLE DATA 2
     const { data, setData, post, patch } = useForm({
-        user_id: auth.user.id,
         proyek_id: proyeks[0].id,
         answer1:
             filteredHasilProyeksById.length != 0
@@ -24,22 +34,27 @@ export default function TahapanProyekSiswa({ auth }) {
             filteredHasilProyeksById.length != 0
                 ? filteredHasilProyeksById[0].answer2
                 : null,
-        answer3:
-            filteredHasilProyeksById.length != 0
-                ? filteredHasilProyeksById[0].answer3
-                : null,
-        answer4:
-            filteredHasilProyeksById.length != 0
-                ? filteredHasilProyeksById[0].answer4
-                : null,
+        answer3: null,
+        answer4: null,
     });
 
     const [uploadedFiles, setUploadedFiles] = useState([]);
 
-    const handleDrop = (index, files) => {
-        const fieldName = `answer${index}`;
-        setData(fieldName, files[0]);
-        setUploadedFiles((prevFiles) => [...prevFiles, ...files]);
+    // ======================================== HANDLE DROP 1
+    // const handleDrop = (index, files) => {
+    //     const fieldName = `answer${index}`;
+    //     setData(fieldName, files[0]);
+    //     setUploadedFiles((prevFiles) => [...prevFiles, ...files]);
+    // };
+
+    // ======================================== HANDLE DROP 2
+    const handleDrop = (files) => {
+        setUploadedFiles(files);
+        setData((prevData) => ({
+            ...prevData,
+            answer2: files[0],
+        }));
+        console.log(uploadedFiles);
     };
 
     const handleProyekStore = (e) => {
@@ -54,6 +69,7 @@ export default function TahapanProyekSiswa({ auth }) {
 
     useEffect(() => {
         console.log("Proyek", proyeks);
+        console.log("Hasil Proyek", hasilProyeks);
         console.log("Hasil By User", filteredHasilProyeksById);
     }, [currentStep]);
 
@@ -74,39 +90,6 @@ export default function TahapanProyekSiswa({ auth }) {
                 }`}
                 type="application/pdf"
             />
-
-            <form className="w-4/5 mx-auto">
-                <textarea
-                    className="w-4/5 block mx-auto border border-black rounded-lg"
-                    placeholder="Masukkan Jawaban..."
-                    name="answer1"
-                    id="answer1"
-                    rows="10"
-                    value={data.answer1}
-                    onChange={(e) => setData("answer1", e.target.value)}
-                ></textarea>
-                <div className="flex gap-12 justify-center mt-16">
-                    <Link
-                        href={route("proyek.index")}
-                        as="button"
-                        className="border border-first bg-white font-semibold py-4 px-16 rounded-lg text-first"
-                    >
-                        Batal
-                    </Link>
-                    <button
-                        type="button"
-                        className="bg-first font-semibold py-4 px-16 rounded-lg text-white"
-                        onClick={
-                            filteredHasilProyeksById.length != 0
-                                ? handleProyekUpdate
-                                : handleProyekStore
-                        }
-                    >
-                        Kirim
-                    </button>
-                </div>
-            </form>
-
             <form className="w-4/5 mx-auto">
                 {/* {currentStep == 1 && ( */}
                 <textarea
@@ -120,22 +103,19 @@ export default function TahapanProyekSiswa({ auth }) {
                 ></textarea>
                 {/* )} */}
                 {/* {currentStep == 2 && ( */}
-                <Upload
-                    onDrop={(files) => handleDrop(2, files)}
-                    uploadedFiles={uploadedFiles}
-                />
+                <Upload onDrop={handleDrop} uploadedFiles={uploadedFiles} />
                 {/* )} */}
                 {/* {currentStep == 3 && ( */}
-                <Upload
+                {/* <Upload
                     onDrop={(files) => handleDrop(3, files)}
                     uploadedFiles={uploadedFiles}
-                />
+                /> */}
                 {/* )} */}
                 {/* {currentStep == 4 && ( */}
-                <Upload
+                {/* <Upload
                     onDrop={(files) => handleDrop(4, files)}
                     uploadedFiles={uploadedFiles}
-                />
+                /> */}
                 {/* )} */}
                 <div className="flex gap-12 justify-center mt-16">
                     <Link
